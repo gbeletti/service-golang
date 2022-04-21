b161f0485f0d6d0b8cec673e3d5611eac6d6e4bd
gbeletti <gbeletti@gmail.com>
2022-04-21 14:53:00 -0300

    basic http server with start and shutdown functions

---
+++ httpserver/http.go
@@ -0,0 +1,43 @@
+package httpserver
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+var srv *http.Server
+
+// Start starts the http server
+func Start() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloWorld)
+	srv = &http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+}
+
+// Shutdown shuts down the http server
+func Shutdown(ctx context.Context) (done chan struct{}) {
+	done = make(chan struct{})
+	go func() {
+		defer close(done)
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("couldnt shutdown server error [%s]\n", err)
+		}
+	}()
+	return
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello World"))
+	if err != nil {
+		log.Printf("couldnt write response error [%s]\n", err)
+	}
+}
