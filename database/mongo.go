85d3bb333fa9b8c28ef156dc0de163f02c3b81c3
gbeletti <gbeletti@gmail.com>
2022-05-26 18:44:04 -0300

    creates package to connect to mongo DB

---
+++ database/mongo.go
@@ -0,0 +1,68 @@
+package database
+
+import (
+	"context"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	client *mongo.Client
+	opt    *options.ClientOptions
+)
+
+// Start should be called on the service start up. Makes the first connection to the database
+func Start(opts ...*options.ClientOptions) (err error) {
+	if len(opts) == 0 {
+		opt = options.Client().ApplyURI(getURI())
+	} else {
+		opt = opts[0]
+	}
+	cli, err := connectDB()
+	if err != nil {
+		return
+	}
+	client = cli
+	return
+}
+
+// GetClient returns a MongoDB client
+func GetClient() (*mongo.Client, error) {
+	if client != nil {
+		return client, nil
+	}
+	cli, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+	client = cli
+	return client, nil
+}
+
+// Close closes all the connections to the database
+func Close(ctx context.Context) (err error) {
+	if client == nil {
+		return
+	}
+	err = client.Disconnect(ctx)
+	return
+}
+
+func connectDB() (cli *mongo.Client, err error) {
+	cli, err = mongo.NewClient(opt)
+	if err != nil {
+		return
+	}
+	err = cli.Connect(context.Background())
+	return
+}
+
+func getURI() (uri string) {
+	uri = os.Getenv("MONGO_URI")
+	if uri == "" {
+		uri = "mongodb://localhost:27017"
+	}
+	return
+}
